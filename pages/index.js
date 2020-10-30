709e4246a8cc3d318d8e689cf6f50976fae6158e
Subraiz <subraizahmed@gmail.com>
2020-10-30 08:04:38 -0400

    loadnig ui

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Index() {
 
     setTimeout(() => {
       const updateLoaderProgress = setInterval(() => {
-        if (progress >= 60) {
+        if (progress >= 80) {
           clearInterval(updateLoaderProgress);
         } else {
           updateProgress((progress += 1));
@@ -62,8 +62,8 @@ export default function Index() {
         { top: "-30px", opacity: 1, ease: "expo.inOut" },
         "-=1"
       )
-      .to(loader, 6, {
-        width: "60%",
+      .to(loader, 8, {
+        width: "80%",
         ease: "linear"
       });
 
@@ -82,14 +82,14 @@ export default function Index() {
 
         tl.kill();
         TweenMax.to(loader, 1, { ease: "linear", width: "100%" });
-        updateProgress(60);
+        updateProgress(80);
         const updateLoaderProgress = setInterval(() => {
           if (progress >= 100) {
             clearInterval(updateLoaderProgress);
           } else {
             updateProgress((progress += 1));
           }
-        }, 25);
+        }, 50);
 
         setTimeout(() => {
           Router.push(`/Account?${queryString}`);
